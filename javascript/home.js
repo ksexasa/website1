e240baa72bd7aacc1b60ea9f2926b83d76bdc176
IliasChatzistogiannis <ioulhatzi@gmail.com>
2024-01-13 02:54:09 +0200

    Added Projects carousel to home page
    Responsive projects carousel with images.

--- javascript/home.js
+++ javascript/home.js
@@ -1,3 +1,54 @@
+var slideIndex = 1;
+var slideshowInterval;
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("project");
+  let dots = document.getElementsByClassName("demo");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+	slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+	dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  if (slideIndex != 1){
+	dots[slideIndex-1].className += " active";
+  }
+}
+
+function slideshow() {
+  let i;
+  let slides = document.getElementsByClassName("project");
+  let dots = document.getElementsByClassName("demo");
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  if (slideIndex < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  if (slideIndex !== 1) {
+    dots[slideIndex - 1].className += " active";
+  }
+  slideIndex++; 
+}
+
+function handleSwipe(startX, endX) {
+  const threshold = 50; // Adjust this value to set the swipe threshold
+
+  if (startX - endX > threshold) {
+    plusSlides(1); // Swipe left, move to the next slide
+  } else if (endX - startX > threshold) {
+    plusSlides(-1); // Swipe right, move to the previous slide
+  }
+}
+
+
 
 window.onload = function () {
 	var togglebutton = document.querySelector(".mobile-nav-toggle");
@@ -16,11 +67,47 @@ window.onload = function () {
 		var herosector = document.querySelector("#Hero");
 		herosector.style.animationName = "carouselhero";
 	}
+	
+	
+	showSlides(slideIndex);
+	slideshowInterval = setInterval(slideshow, 2000);
+
+	const rowElement = document.querySelector('.row');
+	const contElement = document.querySelector('.container');
+
+	let touchStartX = 0;
+
+	rowElement.addEventListener('touchstart', function (e) {
+	  touchStartX = e.touches[0].clientX;
+	});
+	rowElement.addEventListener('touchend', function (e) {
+	  const touchEndX = e.changedTouches[0].clientX;
+	  handleSwipe(touchStartX, touchEndX);
+	});
+	contElement.addEventListener('touchstart', function (e) {
+	  touchStartX = e.touches[0].clientX;
+	});
+	contElement.addEventListener('touchend', function (e) {
+	  const touchEndX = e.changedTouches[0].clientX;
+	  handleSwipe(touchStartX, touchEndX);
+	});
+	
 };
 
 
+function plusSlides(n) {
+  clearInterval(slideshowInterval);
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  clearInterval(slideshowInterval);
+  showSlides(slideIndex = n);
+}
 
-
+	
+	
+	
+	
 
 
 
